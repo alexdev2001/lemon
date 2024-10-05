aad8a317e90b46334851002c428e2e2a1ac34697
alexdev2001 <aleximani201@gmail.com>
2024-10-05 07:30:40 +0200

    some styling changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7664cb84d9181736"; }
+/******/ 		__webpack_require__.h = function() { return "b792abe65af64fd1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
