69a29f6c2ccb64d6f7863f1a380e1f7575390da0
alexdev2001 <aleximani201@gmail.com>
2024-10-06 09:44:59 +0200

    changed some stuff

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -28,11 +28,6 @@ self.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/about.js"
-    ],
-    "/contact": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/contact.js"
     ]
   },
   "ampFirstPages": []
